80ddaee09089c3d94962fa2c14bc20c7ab6c4905
elenamoglan <70642076+elenamoglan@users.noreply.github.com>
2021-01-21 01:14:40 +0200

    Add files via upload

---
+++ Problema.Liste.py
@@ -0,0 +1,10 @@
+a = list(map(int,input('Introduceti numerele listei (prin spatiu) : ').split()))
+print('lista 1 =', a)
+b=sorted(a)
+print('lista 2 =', b)
+c=sorted(a, reverse=True)
+print('lista 3 =', c)
+print(f'Numărul de elemente din listă = {len(a)}, elementul MAX = {max(a)}, elementul MIN = {min(a)}')
+print('lista 4 =', a + [111])
+a[1]=222
+print('lista 5 =', a)
