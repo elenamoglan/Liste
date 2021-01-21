64d8755e80845bff3a290fd871f6275eb5cd6bc9
elenamoglan <70642076+elenamoglan@users.noreply.github.com>
2021-01-21 20:33:12 +0200

    Update Problema.2.Liste.py

--- Problema.2.Liste.py
+++ Problema.2.Liste.py
@@ -2,10 +2,8 @@ with open ('input.txt','r') as f:
     a=list(eval(f.readline()))
 with open ('output.txt', 'w') as f: 
     f.write('Lista 1 = ' + str(a) + '\n')
-    b=sorted(a)
-    f.write('Lista 2 = ' + str(b) + '\n')
-    c=sorted(a, reverse=True)
-    f.write('Lista 3 = ' + str(c) + '\n')
+    f.write('Lista 2 = ' + str(sorted(a)) + '\n')
+    f.write('Lista 3 = ' + str(sorted(a, reverse=True)) + '\n')
     f.write('Numarul de elemente din lista = ' + str(len(a)) + '\n' + 'Elementul MAX = ' + str(max(a)) + '\n' + 'Elementul MIN = ' + str(min(a)) + '\n')
     f.write('Lista 4 = ' + str(a + [111]) + '\n')
     a[1]=222
